ae45bcb5d1a83b689e1aeaa7c37b488e3981e71d
Tigran Asatryan <t.asatryan96@gmail.com>
2023-01-23 13:59:18 +0400

    bcrypt added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,23 @@
-const user = require("../models/User")
+const User = require("../models/User")
+const {StatusCodes} = require("http-status-codes")
+const {BadRequestError} = require ("../errors")
+const bcrypt = require("bcryptjs")
+
 
 
 const register = async(req,res) => {
-    res.send("register user")
+
+     const {name,email,password} = req.body;
+
+     const salt = await bcrypt.genSalt(10);
+     const hashedPassword = await bcrypt.hash(password, salt)
+
+     const tempUser = {name,email,password:hashedPassword}
+    // if(!name || !email || ! password) {
+    //     throw new BadRequestError ("Please provide name email and password")
+    // }
+    const user = await User.create({...tempUser})
+    res.status(StatusCodes.CREATED).json({user})
 } 
 
 
--- models/User.js
+++ models/User.js
@@ -21,7 +21,7 @@ const UserSchema = new mongoose.Schema({
         type:String,
         required:[true, "Please provide the password "],
         minlength:6,
-        maxlength:12,
+        
     }
 })
 

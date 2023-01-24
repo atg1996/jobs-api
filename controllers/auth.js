6d67b43529e1a183722360308770dd042de83a42
Tigran Asatryan <t.asatryan96@gmail.com>
2023-01-25 00:37:37 +0400

    auth middleware added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 require('dotenv');
 const User = require("../models/User")
 const {StatusCodes} = require("http-status-codes")
-const {BadRequestError} = require ("../errors")
+const {BadRequestError, UnauthenticatedError} = require ("../errors")
 
 
 
@@ -15,7 +15,25 @@ const register = async(req,res) => {
 
 
 const login = async(req,res) => {
-    res.send("login user")
+    const {email, password} = req.body;
+    if(!email || !password) {
+        throw new BadRequestError("Please provide email and password")
+    }
+
+    const user = await User.findOne({email})
+
+    if(!user) {
+        throw new UnauthenticatedError("Invalid Credentials")
+    }
+
+    const isPasswordCorrect = await user.comparePasswords(password)
+
+    if(!isPasswordCorrect) {
+        throw new UnauthenticatedError("Invalid Credentials")
+    }
+
+    const token = user.createJwt();
+    res.status(StatusCodes.OK).json({user:{name:user.name}, token})
 }
 
 
